b62c3eb66a81160fc64b8302e2e649090d3b2884
Beanalini <dat826@gmail.com>
2023-12-27 15:18:55 +0000

    feat:add styling to active nav links

--- client/src/components/header/nav.tsx
+++ client/src/components/header/nav.tsx
@@ -4,14 +4,37 @@ const Nav = () => (
   <nav className="nav">
     <ul className="nav__item">
       <li className="nav__item__link">
-        <NavLink to="/"> Home</NavLink>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? "nav__item__link--active" : "nav-link-inActive"
+          }
+        >
+          {" "}
+          Home
+        </NavLink>
       </li>
 
       <li className="nav__item__link">
-        <NavLink to="/confession"> Confessions</NavLink>
+        <NavLink
+          to="/misdemeanour"
+          className={({ isActive }) =>
+            isActive ? "nav__item__link--active" : "nav-link-inActive"
+          }
+        >
+          Misdemeanours
+        </NavLink>
       </li>
       <li className="nav__item__link">
-        <NavLink to="/misdemeanour"> Misdemeanours</NavLink>
+        <NavLink
+          to="/confession"
+          className={({ isActive }) =>
+            isActive ? "nav__item__link--active" : "nav-link-inActive"
+          }
+        >
+          {" "}
+          Confess To Us
+        </NavLink>
       </li>
     </ul>
   </nav>

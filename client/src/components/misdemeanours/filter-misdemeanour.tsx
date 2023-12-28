5a94dd47b62e2bf5697c19ea1ca13903d13f5049
Beanalini <dat826@gmail.com>
2023-12-28 01:34:00 +0000

    feat: add FilterMisdemeanour component and userFilterMisdemeanour function to handle change in slect misdemenour drop down list

---
+++ client/src/components/misdemeanours/filter-misdemeanour.tsx
@@ -0,0 +1,30 @@
+interface FilterMisdemeanourProps {
+  userValue: string;
+  onChangeUserValue: (value: string) => void;
+}
+
+const FilterMisdemeanour: React.FC<FilterMisdemeanourProps> = ({
+  userValue,
+  onChangeUserValue,
+}) => {
+  return (
+    <>
+      <div>
+        <label htmlFor="filter-misdemeanour">Select Misdemeanour</label>
+        <select
+          id="filter-misdemeanour"
+          name="filter-misdemeanour"
+          value={userValue}
+          onChange={(e) => onChangeUserValue(e.target.value)}
+        >
+          <option value="Show All">Show All</option>
+          <option value="rudeness">Rudeness</option>
+          <option value="vegetables">Vegetables</option>
+          <option value="lift">Lift</option>
+          <option value="united">United</option>
+        </select>
+      </div>
+    </>
+  );
+};
+export default FilterMisdemeanour;
--- client/src/components/misdemeanours/misdemeanours.tsx
+++ client/src/components/misdemeanours/misdemeanours.tsx
@@ -1,10 +1,15 @@
 import { useState, useEffect } from "react";
 import { Misdemeanour } from "../../../types/misdemeanours.types";
 import MisdemeanoursTable from "./misdemeanours-table";
+import FilterMisdemeanour from "./filter-misdemeanour";
 
 const Misdemeanours: React.FC = () => {
   const url = `http://localhost:8080/api/misdemeanours/8`;
   const [misdemeanours, setMisdemeanours] = useState<Misdemeanour[]>([]);
+  const [userValue, setUserValue] = useState<string>("Show All");
+  const [displayMisdemeanours, setDisplayMisdemeanours] = useState<
+    Misdemeanour[]
+  >([]);
 
   useEffect(() => {
     const fetchMisdemeanours = async () => {
@@ -18,11 +23,30 @@ const Misdemeanours: React.FC = () => {
       }
     };
     fetchMisdemeanours();
-  }, [url]);
+  }, []);
+
+  const userFilterMisdemeanour = (userMisdemeanour: string) => {
+    //filter misdemeanours if not show all
+    console.log(userMisdemeanour);
+    setUserValue(userMisdemeanour);
+    if (userMisdemeanour !== "Show All") {
+      //filter data by user selection
+      const filteredMisdemeanours = misdemeanours.filter(
+        (misdemeanour) => misdemeanour.misdemeanour === userMisdemeanour
+      );
+      console.log(filteredMisdemeanours);
+      setDisplayMisdemeanours(filteredMisdemeanours);
+    } else {
+      setDisplayMisdemeanours(misdemeanours);
+    }
+  };
   return (
     <>
-      Misdemeanour!
-      <MisdemeanoursTable misdemeanours={misdemeanours} />
+      <FilterMisdemeanour
+        userValue={userValue}
+        onChangeUserValue={(event) => userFilterMisdemeanour(event)}
+      />
+      <MisdemeanoursTable misdemeanours={displayMisdemeanours} />
     </>
   );
 };
